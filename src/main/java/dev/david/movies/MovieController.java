6b18c73ee6f6799d7635d6ba136329493857ce4e
David Newman <davidn988878@gmail.com>
2023-11-25 11:42:21 -0800

    update REST API; working

--- src/main/java/dev/david/movies/MovieController.java
+++ src/main/java/dev/david/movies/MovieController.java
@@ -1,14 +1,33 @@
 package dev.david.movies;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.status.Status;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public String allMovies() {
-        return "All Movies!";
+    public ResponseEntity<List<Movie>> getAllMovies() {
+            return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id) {
+            return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
     }
 }
